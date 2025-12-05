8b9e7025c63738d4d848219220c68c186d1b7882
nathanielalex <nathanalex019@gmail.com>
2025-12-05 15:45:45 +0700

    feat: authenticate using google

--- app/src/main/java/com/example/nurtura/LoginActivity.java
+++ app/src/main/java/com/example/nurtura/LoginActivity.java
@@ -1,14 +1,48 @@
 package com.example.nurtura;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.credentials.ClearCredentialStateRequest;
+import androidx.credentials.CredentialManager;
+import androidx.credentials.CredentialManagerCallback;
+import androidx.credentials.CustomCredential;
+import androidx.credentials.GetCredentialRequest;
+import androidx.credentials.GetCredentialResponse;
+import androidx.credentials.exceptions.ClearCredentialException;
+import androidx.credentials.exceptions.GetCredentialException;
+
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "GoogleSignIn";
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private CredentialManager credentialManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +54,166 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        credentialManager = CredentialManager.create(this);
+
+        db = FirebaseFirestore.getInstance();
+
+        Button signInButton = findViewById(R.id.btnSignIn);
+        signInButton.setOnClickListener(v -> signInWithGoogle());
     }
+
+    private void signInWithGoogle() {
+        String key = BuildConfig.WEB_CLIENT_KEY;
+        GetGoogleIdOption googleIdOption = new GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(false) // Set to false to allow user to pick any account
+                .setServerClientId(key)
+                .setAutoSelectEnabled(true)
+                .build();
+
+        GetCredentialRequest request = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            request = new GetCredentialRequest.Builder().
+                    addCredentialOption(googleIdOption)
+                    .build();
+        }
+
+
+        credentialManager.getCredentialAsync(
+                this,
+                request,
+                new CancellationSignal(),
+                Executors.newSingleThreadExecutor(),
+                new CredentialManagerCallback<GetCredentialResponse, GetCredentialException>() {
+                    @Override
+                    public void onResult(GetCredentialResponse getCredentialResponse) {
+                        handleSignIn(getCredentialResponse);
+                    }
+
+                    @Override
+                    public void onError(@NonNull GetCredentialException e) {
+                        Log.e(TAG, "GetCredential failed", e);
+                        runOnUiThread(() -> Toast.makeText(LoginActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show());
+                    }
+                }
+        );
+
+    }
+
+    private void handleSignIn(GetCredentialResponse result) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            var credential = result.getCredential();
+            if (credential instanceof CustomCredential customCredential && credential.getType().equals(GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL)) {
+                try {
+                    //android vs androidx
+                    Bundle credentialData = customCredential.getData();
+                    GoogleIdTokenCredential googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credentialData);
+
+                    // 5. Authenticate with Firebase using the ID Token
+                    firebaseAuthWithGoogle(googleIdTokenCredential.getIdToken());
+                } catch (Exception e) {
+                    Log.e(TAG, "Error parsing credential", e);
+                }
+            } else {
+                Log.w(TAG, "Credential is not of type Google ID!");
+            }
+        }
+    }
+
+    public static void signOut(Context context) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        CredentialManager credentialManager = CredentialManager.create(context);
+
+        mAuth.signOut();
+        ClearCredentialStateRequest clearRequest = new ClearCredentialStateRequest();
+        credentialManager.clearCredentialStateAsync(
+                clearRequest,
+                new CancellationSignal(),
+                Executors.newSingleThreadExecutor(),
+                new CredentialManagerCallback<Void, ClearCredentialException>() {
+                    @Override
+                    public void onResult(Void unused) {
+                        Log.d(TAG, "Credential state cleared");
+                        //updateUI(null);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ClearCredentialException e) {
+                        Log.e(TAG, "Couldn't clear user credentials: " + e.getLocalizedMessage());
+                    }
+                }
+        );
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        // 6. Exchange Google ID token for Firebase credential
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success
+                        Log.d(TAG, "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if(user != null) {
+                            saveUserToFirestore(user);
+                        }
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser(); //will return the currently signed-in user if they haven’t signed out.
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            // Logged in
+            Log.d(TAG, "Logged in as: " + user.getEmail());
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            Log.d(TAG, "User not logged in");
+        }
+    }
+
+    private void saveUserToFirestore(FirebaseUser user) {
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("uid", user.getUid());
+        userData.put("name", user.getDisplayName());
+        userData.put("email", user.getEmail());
+        userData.put("last_login", com.google.firebase.Timestamp.now());
+
+        // Use set() with Merge to update login time without deleting old data
+        db.collection("users").document(user.getUid())
+                .set(userData, SetOptions.merge())
+                .addOnSuccessListener(aVoid -> {
+                    // Navigate to your Main Activity here
+                    startActivity(new Intent(this, MainActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Log.e(TAG, "Error writing document", e);
+                    // If DB fails, we will still let them in
+                    // so they aren't locked out of the app.
+                    startActivity(new Intent(this, MainActivity.class));
+                    finish();
+                });
+    }
+
 }
--- app/src/main/java/com/example/nurtura/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/nurtura/fragment/ProfileFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.nurtura.LoginActivity;
 import com.example.nurtura.R;
 
 public class ProfileFragment extends Fragment {
@@ -16,6 +18,10 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button signOutButton = view.findViewById(R.id.btnSignOut);
+        signOutButton.setOnClickListener(v -> LoginActivity.signOut(requireContext()));
+        return view;
     }
 }
